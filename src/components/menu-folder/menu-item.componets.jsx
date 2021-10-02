80c4011c450eb6c55d35ccf9f80e6b8db0170028
Ritesh Benjwal <riteshbenjwal7@gmail.com>
2021-10-02 11:18:03 +0530

    BREAKDOWN_HOMEPAGE_INTO_SMALLER_COMPONENTS

---
+++ src/components/menu-folder/menu-item.componets.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./menu-item.styles.scss";
+
+const MenuItem = ({ title, imageUrl, size }) => {
+  return (
+    <div
+      style={{
+        backgroundImage: `url(${imageUrl})`,
+      }}
+      className={`${size} menu-item`}
+    >
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+
+      <div className="content">
+        <div className="title">{title.toUpperCase()}</div>
+        <span className="subtitle">SHOP NOW</span>
+      </div>
+    </div>
+  );
+};
+
+export default MenuItem;
