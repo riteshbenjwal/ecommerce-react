cbafa0bb7103d737f548a2df92047bc582662e8f
Ritesh Benjwal <riteshbenjwal7@gmail.com>
2021-10-28 09:21:07 +0530

    Redux_PERSISTENCE_COLLECTION_OVERVIEW

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,34 +1,18 @@
 import React, { Component } from "react";
-import SHOP_DATA from "./shop.data.js";
-import CollectionPreview from "../../components/preview-collection/collection-preview.jsx";
+import CollectionOverview from '../../components/collections-overview/collections-overview.component'
 
-class ShopPage extends Component {
-  constructor(props) {
-    super(props);
+const ShopPage =({collections}) => {
 
-    this.state = {
-      collections: SHOP_DATA,
-    };
-  }
-
-
-
-  render() {
-    const {collections} = this.state;
       return(
         <div className='shop-page'>
-       {
-         collections.map(({id, ...otherCollectionProps}) =>(
-           <CollectionPreview key={id} {...otherCollectionProps}/>
-         ))
-       }
+     <CollectionOverview/>
         </div>
 
       )
-  }
-
-
 
 }
+
+
+
 
 export default ShopPage;
